7c6aa4e996e0e2797331dbc32f2bc13a9b335181
Gemma Shearer <gemmaandjude@gmail.com>
2015-05-29 13:02:40 +0100

    Initial Commit

---
+++ Shoe Converter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shoe Converter
+//
+//  Created by Gemma Shearer on 29/05/2015.
+//  Copyright (c) 2015 GemmaShearer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
